5f66011fed8f26a704fd1bbc25f294383ddebf07
Hiram K. <hiram.kamau@outlook.com>
2022-04-26 22:30:01 +0300

    Create MainViewController.java

---
+++ src/main/java/com/github/idelstak/snakefx/controllers/MainViewController.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License
+ * Copyright © 2022 Hiram K
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.idelstak.snakefx.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+public class MainViewController {
+
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private URL location;
+    @FXML
+    private BorderPane mainPane;
+    @FXML
+    private HBox topBox;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void initialize() {
+        System.out.println("Loaded fxml file from: " + location);
+        
+        exitButton.visibleProperty().bind(settingsButton.visibleProperty());
+        backButton.visibleProperty().bind(settingsButton.visibleProperty().not());
+        saveButton.visibleProperty().bind(exitButton.visibleProperty().not());
+    }
+
+    @FXML
+    void displaySettings(ActionEvent event) {
+        settingsButton.setVisible(false);
+        event.consume();
+    }
+
+    @FXML
+    void backToMain(ActionEvent event) {
+        settingsButton.setVisible(true);
+        event.consume();
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+        event.consume();
+    }
+
+    @FXML
+    void saveSettings(ActionEvent event) {
+        event.consume();
+    }
+
+}
