fb77dc3f8f2475aad76a0f14cf4c949409ad63b1
Hiram K. <hiram.kamau@outlook.com>
2022-04-26 14:48:06 +0300

    Update SettingsViewController.java

--- src/main/java/com/github/idelstak/snakefx/SettingsViewController.java
+++ src/main/java/com/github/idelstak/snakefx/SettingsViewController.java
@@ -22,17 +22,75 @@
  */
 package com.github.idelstak.snakefx;
 
-import javafx.scene.layout.Region;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.util.Builder;
 
-class SettingsViewController implements Builder<Region>{
+class SettingsViewController implements Builder<Node> {
 
     SettingsViewController() {
     }
 
     @Override
-    public Region build() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Node build() {
+        var hardRB = new RadioButton("Hard");
+        var mediumRB = new RadioButton("Medium");
+        var easyRB = new RadioButton("Easy");
+
+        new ToggleGroup().getToggles().addAll(hardRB, mediumRB, easyRB);
+
+        var difficultyBox = new HBox(hardRB, mediumRB, easyRB);
+        difficultyBox.setSpacing(6);
+
+        var levelBox = new VBox(new Text("Difficulty"), difficultyBox);
+        levelBox.setSpacing(6);
+
+        var up = new Label("Up:");
+        up.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(up, Priority.ALWAYS);
+
+        var down = new Label("Down:");
+        down.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(down, Priority.ALWAYS);
+
+        var left = new Label("Left:");
+        left.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(left, Priority.ALWAYS);
+
+        var right = new Label("Right:");
+        right.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(right, Priority.ALWAYS);
+
+        var upBox = new HBox(up, new Text("W"));
+        var downBox = new HBox(down, new Text("S"));
+        var leftBox = new HBox(left, new Text("A"));
+        var rightBox = new HBox(right, new Text("D"));
+
+        var controlsBox = new VBox(new Text("Controls"), upBox, downBox, leftBox, rightBox);
+        controlsBox.setBorder(new Border(new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, new CornerRadii(12), null)));
+        controlsBox.setSpacing(6);
+        controlsBox.setPadding(new Insets(6));
+
+        var innerBox = new VBox(controlsBox, levelBox);
+        innerBox.setSpacing(24);
+        innerBox.setPadding(new Insets(0, 18, 18, 18));
+
+        var outerBox = new VBox(innerBox);
+        outerBox.setPadding(new Insets(24));
+
+        return outerBox;
     }
 
 }
