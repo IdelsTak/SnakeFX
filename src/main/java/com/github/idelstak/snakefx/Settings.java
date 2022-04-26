869b095b848cc0ad2eec4134d207eca30689fe9c
Hiram K. <hiram.kamau@outlook.com>
2022-04-26 16:02:47 +0300

    Update Settings.java

--- src/main/java/com/github/idelstak/snakefx/Settings.java
+++ src/main/java/com/github/idelstak/snakefx/Settings.java
@@ -24,16 +24,29 @@ package com.github.idelstak.snakefx;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.input.KeyCode;
 
-class Settings {
+public class Settings {
 
-    private final ObjectProperty<Difficulty> difficultyProperty = new SimpleObjectProperty<>(this, "Difficulty property", Difficulty.EASY);
+    private final ObjectProperty<Difficulty> difficultyProperty;
+    private final ObjectProperty<KeyCode> upKeyCodeProperty;
+    private final ObjectProperty<KeyCode> downKeyCodeProperty;
+    private final ObjectProperty<KeyCode> leftKeyCodeProperty;
+    private final ObjectProperty<KeyCode> rightKeyCodeProperty;
 
-    Difficulty getDifficulty() {
+    public Settings() {
+        this.difficultyProperty = new SimpleObjectProperty<>(this, "Difficulty property", Difficulty.EASY);
+        this.upKeyCodeProperty = new SimpleObjectProperty<>(this, "Up key property", KeyCode.W);
+        this.downKeyCodeProperty = new SimpleObjectProperty<>(this, "Down key property", KeyCode.S);
+        this.leftKeyCodeProperty = new SimpleObjectProperty<>(this, "Left key property", KeyCode.A);
+        this.rightKeyCodeProperty = new SimpleObjectProperty<>(this, "Right key property", KeyCode.D);
+    }
+
+    public Difficulty getDifficulty() {
         return difficultyProperty.get();
     }
 
-    void setDifficulty(Difficulty difficulty) {
+    public void setDifficulty(Difficulty difficulty) {
         difficultyProperty.set(difficulty);
     }
 
@@ -41,8 +54,52 @@ class Settings {
         return difficultyProperty;
     }
 
-    enum Difficulty {
-        EASY, MEDIUM, HARD
+    public KeyCode getUpKeyCode() {
+        return upKeyCodeProperty.get();
+    }
+
+    public void setUpKeyCode(KeyCode upKeyCode) {
+        upKeyCodeProperty.set(upKeyCode);
+    }
+
+    public ObjectProperty<KeyCode> upKeyCodeProperty() {
+        return upKeyCodeProperty;
+    }
+
+    public KeyCode getDownKeyCode() {
+        return downKeyCodeProperty.get();
+    }
+
+    public void setDownKeyCode(KeyCode downKeyCode) {
+        downKeyCodeProperty.set(downKeyCode);
+    }
+
+    public ObjectProperty<KeyCode> downKeyCodeProperty() {
+        return downKeyCodeProperty;
+    }
+
+    public KeyCode getLeftKeyCode() {
+        return leftKeyCodeProperty.get();
+    }
+
+    public void setLeftKeyCode(KeyCode leftKeyCode) {
+        leftKeyCodeProperty.set(leftKeyCode);
+    }
+
+    public ObjectProperty<KeyCode> leftKeyCodeProperty() {
+        return leftKeyCodeProperty;
+    }
+
+    public KeyCode getRightKeyCode() {
+        return rightKeyCodeProperty.get();
+    }
+
+    public void setRightKeyCode(KeyCode rightKeyCode) {
+        rightKeyCodeProperty.set(rightKeyCode);
+    }
+
+    public ObjectProperty<KeyCode> rightKeyCodeProperty() {
+        return rightKeyCodeProperty;
     }
 
     @Override
@@ -50,4 +107,7 @@ class Settings {
         return "Settings {difficulty = %s}".formatted(getDifficulty());
     }
 
+    public enum Difficulty {
+        EASY, MEDIUM, HARD
+    }
 }
