8ec743166ca3dfc5ca4c0a3d50b25bcd7c6244c2
Hiram K. <hiram.kamau@outlook.com>
2022-04-26 13:27:19 +0300

    Update SnakeViewBuilder.java

--- src/main/java/com/github/idelstak/snakefx/SnakeViewBuilder.java
+++ src/main/java/com/github/idelstak/snakefx/SnakeViewBuilder.java
@@ -1,5 +1,6 @@
 package com.github.idelstak.snakefx;
 
+import java.util.function.Consumer;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
@@ -8,6 +9,12 @@ import javafx.scene.layout.Region;
 import javafx.util.Builder;
 
 class SnakeViewBuilder implements Builder<Region> {
+
+    private final Consumer<Runnable> settingsDisplay;
+
+    SnakeViewBuilder(Consumer<Runnable> settingsDisplay) {
+        this.settingsDisplay = settingsDisplay;
+    }
 
     @Override
     public Region build() {
@@ -21,7 +28,15 @@ class SnakeViewBuilder implements Builder<Region> {
 
     private Node menu() {
         var play = new Button("Play");
+
         var settings = new Button("Settings");
+        settings.setOnAction(evt -> {
+            settings.setDisable(true);
+            settingsDisplay.accept(() -> {
+                settings.setDisable(false);
+                evt.consume();
+            });
+        });
         var exit = new Button("Exit");
         var toolBar = new ToolBar(play, settings, exit);
 
