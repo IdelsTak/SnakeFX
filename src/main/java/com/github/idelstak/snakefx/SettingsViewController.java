455b1ad07ca4164a2e497450b322502d06cbb09f
Hiram K. <hiram.kamau@outlook.com>
2022-04-26 15:37:13 +0300

    Update SettingsViewController.java

--- src/main/java/com/github/idelstak/snakefx/SettingsViewController.java
+++ src/main/java/com/github/idelstak/snakefx/SettingsViewController.java
@@ -22,10 +22,16 @@
  */
 package com.github.idelstak.snakefx;
 
+import com.github.idelstak.snakefx.Settings.Difficulty;
+import java.util.Objects;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
@@ -38,18 +44,50 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.util.Builder;
 
-class SettingsViewController implements Builder<Node> {
+class SettingsViewController implements Builder<Dialog<Settings.Difficulty>> {
 
-    SettingsViewController() {
+    private final RadioButton easyRB;
+    private final RadioButton mediumRB;
+    private final RadioButton hardRB;
+    private final ToggleGroup toggleGroup;
+    private final Settings settings;
+
+    SettingsViewController(Settings settings) {
+        this.settings = settings;
+        easyRB = new RadioButton("Easy");
+        mediumRB = new RadioButton("Medium");
+        hardRB = new RadioButton("Hard");
+        toggleGroup = new ToggleGroup();
     }
 
     @Override
-    public Node build() {
-        var hardRB = new RadioButton("Hard");
-        var mediumRB = new RadioButton("Medium");
-        var easyRB = new RadioButton("Easy");
+    public Dialog<Difficulty> build() {
+        var dialog = new Dialog<Difficulty>();
+        var dialogPane = dialog.getDialogPane();
 
-        new ToggleGroup().getToggles().addAll(hardRB, mediumRB, easyRB);
+        dialogPane.setContent(content());
+        dialogPane.getButtonTypes().setAll(
+                new ButtonType[]{
+                    new ButtonType("Save", ButtonBar.ButtonData.OK_DONE),
+                    new ButtonType("Return", ButtonBar.ButtonData.CANCEL_CLOSE)
+                }
+        );
+        
+        this.setSelectedItem(settings.getDifficulty());
+
+        dialog.setDialogPane(dialogPane);
+
+        dialog.setResultConverter(dialogButton -> {
+            ButtonBar.ButtonData data = dialogButton == null ? null : dialogButton.getButtonData();
+            return data == ButtonBar.ButtonData.OK_DONE ? this.getSelectedItem() : null;
+        });
+
+        return dialog;
+    }
+
+    private Node content() {
+
+        toggleGroup.getToggles().setAll(hardRB, mediumRB, easyRB);
 
         var difficultyBox = new HBox(hardRB, mediumRB, easyRB);
         difficultyBox.setSpacing(6);
@@ -91,6 +129,36 @@ class SettingsViewController implements Builder<Node> {
         outerBox.setPadding(new Insets(24));
 
         return outerBox;
+    }
+
+    private Difficulty getSelectedItem() {
+        var selected = toggleGroup.getToggles()
+                .stream()
+                .filter(Toggle::isSelected)
+                .map(RadioButton.class::cast)
+                .findFirst()
+                .orElseThrow();
+
+        if (Objects.equals(easyRB, selected)) {
+            return Difficulty.EASY;
+        } else if (Objects.equals(mediumRB, selected)) {
+            return Difficulty.MEDIUM;
+        } else {
+            return Difficulty.HARD;
+        }
+    }
+
+    private void setSelectedItem(Difficulty difficulty) {
+        switch (difficulty) {
+            case EASY ->
+                easyRB.setSelected(true);
+            case MEDIUM ->
+                mediumRB.setSelected(true);
+            case HARD ->
+                hardRB.setSelected(true);
+            default ->
+                easyRB.setSelected(true);
+        }
     }
 
 }
