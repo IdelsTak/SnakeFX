eedb5ef121cb523c0d6a8cb15c1714e27d40e4c5
Hiram K. <hiram.kamau@outlook.com>
2022-04-26 13:34:07 +0300

    Update SnakeViewBuilder.java

--- src/main/java/com/github/idelstak/snakefx/SnakeViewBuilder.java
+++ src/main/java/com/github/idelstak/snakefx/SnakeViewBuilder.java
@@ -1,3 +1,25 @@
+/*
+ * The MIT License
+ * Copyright © 2022 Hiram K
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.github.idelstak.snakefx;
 
 import java.util.function.Consumer;
@@ -11,6 +33,7 @@ import javafx.util.Builder;
 class SnakeViewBuilder implements Builder<Region> {
 
     private final Consumer<Runnable> settingsDisplay;
+    private BorderPane gamePane;
 
     SnakeViewBuilder(Consumer<Runnable> settingsDisplay) {
         this.settingsDisplay = settingsDisplay;
@@ -18,12 +41,12 @@ class SnakeViewBuilder implements Builder<Region> {
 
     @Override
     public Region build() {
-        var pane = new BorderPane();
+        gamePane = new BorderPane();
 
-        pane.setPrefSize(500, 400);
-        pane.setTop(menu());
+        gamePane.setPrefSize(500, 400);
+        gamePane.setTop(menu());
 
-        return pane;
+        return gamePane;
     }
 
     private Node menu() {
@@ -32,6 +55,9 @@ class SnakeViewBuilder implements Builder<Region> {
         var settings = new Button("Settings");
         settings.setOnAction(evt -> {
             settings.setDisable(true);
+            
+            gamePane.setCenter(new SettingsViewController().build());
+
             settingsDisplay.accept(() -> {
                 settings.setDisable(false);
                 evt.consume();
